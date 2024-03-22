7c57403d85c4cb87ac03cbba7bbf2d32bc3b5814
oliinykroman <oliinykroman@gmail.com>
2024-03-22 15:30:13 +0200

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,26 @@ $(document).ready(function () {
   $('.trusted-slider').slick({
     slidesToShow: 7,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
-          arrows: false,
-          centerMode: true,
-          autoplay: true,
-          autoplaySpeed: 2000,
+          slidesToScroll: 1,
         }
       },
       {
         breakpoint: 767,
         settings: {
           slidesToShow: 3,
-          arrows: false,
-          centerMode: true,
-          autoplay: true,
-          autoplaySpeed: 2000,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
         }
       }
     ]
@@ -48,8 +47,9 @@ $(document).ready(function () {
           breakpoint: 767,
           settings: {
             slidesToShow: 1,
+            arrows: false,
           }
-        },
+        }
       ]
     });
   });
