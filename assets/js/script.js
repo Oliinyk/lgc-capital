7119744d5d743fe8f63744948f1115f30b2f6c6d
oliinykroman <oliinykroman@gmail.com>
2024-03-23 00:30:17 +0200

    merged

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,73 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
   // trusted-slider
-  $('.trusted-slider').slick({
-    slidesToShow: 7,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-        }
-      }
-    ]
-  });
-
-  //review-slider
-  $('.review-slider').each(function() {
-    $( this ).slick({
-      slidesToShow: 3,
+  if($('.trusted-slider').length) {
+    $('.trusted-slider').slick({
+      slidesToShow: 7,
       slidesToScroll: 1,
-      arrows: true,
-      dots: true,
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 2000,
       responsive: [
         {
-          breakpoint: 1199,
+          breakpoint: 1024,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 4,
           }
         },
         {
           breakpoint: 767,
           settings: {
-            slidesToShow: 1,
-            arrows: false,
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
           }
         }
       ]
     });
-  });
+  }
 
-  $(document).ready(function(){
-    // drag & drop
-    var $fileInput = $('.file-input');
-    var $droparea = $('.file-drop-area');
-
-    // highlight drag area
-    $fileInput.on('dragenter focus click', function() {
-      $droparea.addClass('is-active');
+  //review-slider
+  if($('.review-slider').length) {
+    $('.review-slider').each(function() {
+      $( this ).slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+          {
+            breakpoint: 1199,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              arrows: false,
+            }
+          }
+        ]
+      });
     });
-
-    // back to normal state
-    $fileInput.on('dragleave blur drop', function() {
-      $droparea.removeClass('is-active');
-    });
-
-    // change inner text
-    $fileInput.on('change', function() {
-      var filesCount = $(this)[0].files.length;
-      var $textContainer = $(this).prev();
-
-      if (filesCount === 1) {
-        // if single file is selected, show file name
-        var fileName = $(this).val().split('\\').pop();
-        $textContainer.text(fileName);
-      } else {
-        // otherwise show number of files
-        $textContainer.text(filesCount + ' files selected');
-      }
-    });
-  });
+  }
 
   // modal
-  $(document).ready(function(){
-    $('.modal-toggle').on('click', function(e) {
-      e.preventDefault();
+  $('.modal-toggle').on('click', function(e) {
+    e.preventDefault();
 
-      $('.modal-form .iconPlacejs img').remove();
-      let checkedId = $('input[type=radio]:checked').attr('id');
-      $('<img src="assets/images/'+checkedId+'.svg" alt="'+checkedId+'">').appendTo('.modal-form .iconPlacejs');
+    $('.modal-form .iconPlacejs img').remove();
+    let checkedId = $('input[type=radio]:checked').attr('id');
+    $('<img src="assets/images/'+checkedId+'.svg" alt="'+checkedId+'">').appendTo('.modal-form .iconPlacejs');
 
-      $('.modal').toggleClass('is-visible');
-    });
+    $('.modal').toggleClass('is-visible');
   });
 
 });
