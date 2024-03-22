b6f19be1ebae38e7ab84ef31a5a428f7e99bbbb3
oliinykroman <oliinykroman@gmail.com>
2024-03-22 23:21:16 +0200

    added modal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,4 +55,48 @@ $(document).ready(function () {
     });
   });
 
+  $(document).ready(function(){
+    // drag & drop
+    var $fileInput = $('.file-input');
+    var $droparea = $('.file-drop-area');
+
+    // highlight drag area
+    $fileInput.on('dragenter focus click', function() {
+      $droparea.addClass('is-active');
+    });
+
+    // back to normal state
+    $fileInput.on('dragleave blur drop', function() {
+      $droparea.removeClass('is-active');
+    });
+
+    // change inner text
+    $fileInput.on('change', function() {
+      var filesCount = $(this)[0].files.length;
+      var $textContainer = $(this).prev();
+
+      if (filesCount === 1) {
+        // if single file is selected, show file name
+        var fileName = $(this).val().split('\\').pop();
+        $textContainer.text(fileName);
+      } else {
+        // otherwise show number of files
+        $textContainer.text(filesCount + ' files selected');
+      }
+    });
+  });
+
+  // modal
+  $(document).ready(function(){
+    $('.modal-toggle').on('click', function(e) {
+      e.preventDefault();
+
+      $('.modal-form .iconPlacejs img').remove();
+      let checkedId = $('input[type=radio]:checked').attr('id');
+      $('<img src="assets/images/'+checkedId+'.svg" alt="'+checkedId+'">').appendTo('.modal-form .iconPlacejs');
+
+      $('.modal').toggleClass('is-visible');
+    });
+  });
+
 });
